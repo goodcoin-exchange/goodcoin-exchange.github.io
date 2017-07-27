2f01965ffb272c765d249546f3e4a7d8d855c60b
worldMachine <kbdavis77@gmail.com>
2017-07-27 16:09:30 -0700

    tictactoe still has problems...

--- js/tictactoe2.js
+++ js/tictactoe2.js
@@ -1,3 +1,7 @@
+var btn1P = false, btn2P = false, btn3P = false, btn4P = false, btn5P = false, btn6P = false, btn7P = false, btn8P = false, btn9P = false;
+
+var winnerFound = false;
+
 var moveCounter = 0;
 
 var userMoves = [];
@@ -55,9 +59,7 @@ function checkWin(pos1, pos2, pos3, playerMoves) {
     var playerWins;
     if ((playerMoves.includes(pos1)) && (playerMoves.includes(pos2)) && (playerMoves.includes(pos3))) {
         playerWins = true;
-        var btn1 = numToBtn(pos1);
-        var btn2 = numToBtn(pos2);
-        var btn3 = numToBtn(pos3);
+        var btn1 = numToBtn(pos1), btn2 = numToBtn(pos2), btn3 = numToBtn(pos3);
         document.getElementById(btn1).style.color = 'red';
         document.getElementById(btn2).style.color = 'red';
         document.getElementById(btn3).style.color = 'red';
@@ -73,6 +75,10 @@ function checkWinner(playerMoves) {
     for (i = 0; i < 8; i++) {
         win = checkWin(winnerPos[i][0], winnerPos[i][1], winnerPos[i][2], playerMoves);
         if (win){
+            btn1P = true, btn2P = true, btn3P = true, btn4P = true, btn5P = true, btn6P = true, btn7P = true, btn8P = true, btn9P = true;
+            var mBtn = document.getElementById('playAgainBtn');
+            mBtn.style.display = 'block';
+            winnerFound = true;
             document.getElementById('message').innerHTML = "holy crap theres a win on the board!";
             i = 9;
         }
@@ -81,9 +87,38 @@ function checkWinner(playerMoves) {
     return win;
 }
 
+
+// function killBtn(num) {
+//     switch (num) {
+//     case '1':
+//         btn1P = true;
+//         break;
+//     case '2':
+//         btn2P = true;
+//     case '3':
+//         btn3P = true;
+//     case '4':
+//         btn4P = true;
+//     case '5':
+//         btn5P = true;
+//     case '6':
+//         btn6P = true;
+//     case '7':
+//         btn7P = true;
+//     case '8':
+//         btn8P = true;
+//     case '9':
+//         btn9P = true;
+//     default:
+//         var def = num;
+//     }
+// }
+
 function populate(num, letter, playerMoves) {
     var btn;
+    // killBtn(num);
     if (moves.includes(num)) {
+
         btn = numToBtn(num);
         document.getElementById(btn).innerHTML = letter;
         playerMoves.push(num);
@@ -93,37 +128,10 @@ function populate(num, letter, playerMoves) {
         if (index > -1) {
             moves.splice(index, 1);
         }
+
     }
 }
 
-function think() {
-    if (moveCounter === 0) {
-        populate('5', 'O', myMoves);
-    } else if (moveCounter === 1){
-        if (userMoves.includes('5')) {
-            populate('1', 'O', myMoves);
-        } else {
-            populate('5', 'O', myMoves);
-        }
-    } else {
-        var answer = anticipateWin();
-        if (answer === '0') {
-            answer = anticipateUserWin();
-            if (answer === '0') {
-                var randomNum = moves[Math.floor(Math.random() * moves.length)];
-                populate(randomNum, 'O', myMoves);
-                document.getElementById('message2').innerHTML = "This is a random move...";
-            } else {
-                populate(answer, 'O', myMoves);
-                document.getElementById('message2').innerHTML = "anticipated USER win.";
-            }
-        } else {
-            populate(answer, 'O', myMoves);
-            document.getElementById('message2').innerHTML = "anticipated MY win.";
-        }
-    }
-
-}
 
 function intersect(a, b)
 {
@@ -142,13 +150,48 @@ function intersect(a, b)
 }
 
 function anticipateWin() {
-    var answer = anticipate(winnerPos, myMoves);
+    document.getElementById('message').innerHTML = "anticipatingWin";
+    var answer = anticipate2(winnerPos, myMoves);
     return answer;
 }
 
 function anticipateUserWin() {
-    var answer = anticipate(winnerPos, userMoves);
+    var answer = anticipate2(winnerPos, userMoves);
     return answer;
+}
+
+function anticipate2 (posList, playerMoves) {
+    var answer = '0';
+    var counter = 0;
+    var sum = 0;
+    var tempArr = [];
+
+    for (lis in posList) {
+        if (answer === '0') {
+            counter = 0;
+            sum = 0;
+            tempArr = [];
+            for (move in playerMoves) {
+                if (lis.includes(move)) {
+                    tempArr.push(move);
+                    if (tempArr.length > 1) {
+                        document.getElementById('message').innerHTML = "yes";
+                        for (i in lis) {
+                            for (j in tempArr) {
+                                if (j !== i) {
+                                    answer = i;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return answer;
+
 }
 
 function anticipate(posList, playerMoves) {
@@ -172,10 +215,36 @@ function anticipate(posList, playerMoves) {
     return answer;
 }
 
-function userFirstBtn() {
-    hideFirstBtns();
-    document.getElementById('message').innerHTML = "Make your move...";
+function think() {
+    if (moveCounter === 0) {
+        populate('5', 'O', myMoves);
+    } else if (moveCounter === 1){
+        if (userMoves.includes('5')) {
+            populate('1', 'O', myMoves);
+        } else {
+            populate('5', 'O', myMoves);
+        }
+    } else {
+
+        var answer = anticipateWin();
+        if (answer === '0') {
+            answer = anticipateUserWin();
+            if (answer === '0') {
+                var randomNum = moves[Math.floor(Math.random() * moves.length)];
+                populate(randomNum, 'O', myMoves);
+                document.getElementById('message2').innerHTML = "This is a random move...";
+            } else {
+                populate(answer, 'O', myMoves);
+                document.getElementById('message2').innerHTML = "anticipated USER win.";
+            }
+        } else {
+            populate(answer, 'O', myMoves);
+            document.getElementById('message2').innerHTML = "anticipated MY win.";
+        }
+    }
+
 }
+
 
 function meFirstBtn() {
     hideFirstBtns();
@@ -183,54 +252,96 @@ function meFirstBtn() {
 }
 
 function hideFirstBtns() {
-    var uBtn = document.getElementById('userFirstBtn');
     var mBtn = document.getElementById('meFirstBtn');
-    uBtn.style.display = 'none';
     mBtn.style.display = 'none';
 }
 
 function buttonOne() {
-    hideFirstBtns();
-    populate('1', 'X', userMoves);
-    think();
+    if (!btn1P){
+        btn1P = true;
+        hideFirstBtns();
+        populate('1', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonTwo() {
-    hideFirstBtns();
-    populate('2', 'X', userMoves);
-    think();
+    if (!btn2P) {
+        btn2P = true;
+        hideFirstBtns();
+        populate('2', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonThree() {
-    hideFirstBtns();
-    populate('3', 'X', userMoves);
-    think();
+    if (!btn3P) {
+        btn3P = true;
+        hideFirstBtns();
+        populate('3', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonFour() {
-    hideFirstBtns();
-    populate('4', 'X', userMoves);
-    think();
+    if (!btn4P) {
+        btn4P = true;
+        hideFirstBtns();
+        populate('4', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonFive() {
-    hideFirstBtns();
-    populate('5', 'X', userMoves);
-    think();
+    if (!btn5P){
+        btn5P = true;
+        hideFirstBtns();
+        populate('5', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonSix() {
-    hideFirstBtns();
-    populate('6', 'X', userMoves);
-    think();
+    if (!btn6P) {
+        hideFirstBtns();
+        populate('6', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonSeven() {
-    hideFirstBtns();
-    populate('7', 'X', userMoves);
-    think();
+    if (!btn7P) {
+        btn7P = true;
+        hideFirstBtns();
+        populate('7', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonEight() {
-    hideFirstBtns();
-    populate('8', 'X', userMoves);
-    think();
+    if (!btn8P) {
+        btn8P = true;
+        hideFirstBtns();
+        populate('8', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
 function buttonNine() {
-    hideFirstBtns();
-    populate('9', 'X', userMoves);
-    think();
+    if (!btn9P) {
+        btn9P = true;
+        hideFirstBtns();
+        populate('9', 'X', userMoves);
+        if (!winnerFound){
+            think();
+        }
+    }
 }
