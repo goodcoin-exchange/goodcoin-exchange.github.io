4f6ea9b81571467275433fc94461203a22a1f8bc
worldMachine <kbdavis77@gmail.com>
2017-10-12 16:26:20 -0400

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 menuOpen = false;
+accountOpen = false;
 
 
 var mq = window.matchMedia( "(max-width: 767px)" );
@@ -8,6 +9,7 @@ window.onload = function() {
   if (mq.matches) {
     // window width is at least 500px
     document.getElementById("mySidenav").style.color = "white";
+    document.getElementById("myaccountnav").style.color = "white";
     setTransparent();
   }
   else {
@@ -21,16 +23,21 @@ window.onload = function() {
 
 function setTransparent(){
      document.getElementById("mySidenav").style.backgroundColor = "transparent";
+     document.getElementById("myaccountnav").style.backgroundColor = "transparent";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.backgroundColor = "transparent";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("topnav").style.backgroundImage = "none";
+  //pushes main content
+  menuOpen = false;
 }
 
 function openNav() {
     if (menuOpen) {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("mySidenav").style.backgroundColor = "transparent";
-        document.getElementById("main").style.marginLeft= "0";
-        document.getElementById("topnav").style.backgroundImage = "none";
-        //pushes main content
-        menuOpen = false;
+        closeNav();
     }
     else {
         mq = window.matchMedia( "(max-width: 767px)" );
@@ -47,8 +54,45 @@ function openNav() {
         else {
         document.getElementById("mySidenav").style.width = "200px";
         document.getElementById("main").style.marginLeft = "200px";
+        //document.getElementById("mySidenav").style.textAlign = "center";
         //pushes main content
         }
+        closeAccount();
         menuOpen = true;
     }
+}
+
+function closeAccount() {
+  document.getElementById("myaccountnav").style.width = "0";
+  document.getElementById("myaccountnav").style.backgroundColor = "transparent";
+  document.getElementById("main-inner").style.marginRight= "0";
+  //document.getElementById("topnav").style.backgroundImage = "none";
+  accountOpen = false;
+}
+
+function openAccount() {
+  if (accountOpen) {
+    closeAccount();
+  }
+  else {
+
+    mq = window.matchMedia( "(max-width: 767px)" );
+    if (mq.matches){
+      document.getElementById("myaccountnav").style.backgroundColor = "#00CCFF";
+      document.getElementById("myaccountnav").style.opacity = "0.9";
+      document.getElementById("myaccountnav").style.textAlign = "center";
+      document.getElementById("myaccountnav").style.height = "100%";
+      document.getElementById("myaccountnav").style.width = "100%";
+      //document.getElementById("topnav").style.backgroundImage = "url('../resources/blue-edu.png')";
+    }
+
+    else {
+      document.getElementById("myaccountnav").style.width = "200px";
+      document.getElementById("main-inner").style.marginRight = "200px";
+      //document.getElementById("myaccountnav").style.textAlign = "center";
+      //pushes main content
+    }
+    closeNav();
+    accountOpen = true;
+  }
 }
